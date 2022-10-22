6ec29b7765a674c6881b1385c8ab07d5f170aa2b
Alina Cherkasova <alina.ch1202@gmail.com>
2022-10-22 22:52:07 +0300

    refactor: components style

--- src/components/Phonebook/Phonebook.styled.js
+++ src/components/Phonebook/Phonebook.styled.js
@@ -1,44 +1,58 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Form, Field } from 'formik';
 
+const APP_WIDTH = 300;
+const BG_COLOR = '#ffffff';
+
+const borderShadow = css`
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+`;
+
 export const Title = styled.p`
-  font-size: ${props => `${props.fontSize}rem` || '1.1rem'};
+  font-size: ${props => `${props.fontSize}rem` || '1rem'};
   font-weight: bold;
   text-transform: uppercase;
 `;
 
 export const ContactsListStyle = styled.ul`
+  width: ${APP_WIDTH}px;
   list-style: none;
-  font-size: 1.2rem;
 `;
 
 export const InputSyled = styled(Field)`
-  padding: 10px 15px;
-  margin: 10px 0;
+  ${borderShadow}
+  padding: 5px;
+  margin-bottom: 10px;
+`;
+
+export const FilterStyled = styled.input`
+  ${borderShadow}
+  padding: 5px;
+  width: ${APP_WIDTH}px;
 `;
 
 export const FormStyled = styled(Form)`
+  ${borderShadow}
   display: flex;
   flex-direction: column;
-  background-color: #f5f5f5;
-  border: 1px solid black;
-  width: 400px;
-  border-radius: 3px;
-  padding: 15px;
+  background-color: ${BG_COLOR};
+  width: ${APP_WIDTH}px;
+  padding: 8px;
 `;
 
 export const ButtonStyle = styled.button`
+  ${borderShadow}
   align-self: center;
-  font-size: 1.1rem;
-  background-color: #e5e5e5;
-  border: 1px solid black;
-  padding: 10px;
+  background-color: ${BG_COLOR};
+  padding: 5px 9px;
   outline: none;
   cursor: pointer;
   transition: all 0.3s;
 
   &:hover {
-    background-color: #eeeeee;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.4);
   }
 
   &:active {
