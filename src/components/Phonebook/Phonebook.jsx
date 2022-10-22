ee74c748fb47e11e04031c600b7810bbef193cb8
Alina Cherkasova <alina.ch1202@gmail.com>
2022-10-22 14:27:52 +0300

    add: phone number to contact

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -10,27 +10,34 @@ import {
 
 const initialValues = {
   name: '',
+  number: '',
 };
 
 export class Phonebook extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   handleChange = e => {
-    this.setState({ name: e.currentTarget.value });
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
   };
 
   handleSubmit = (values, actions) => {
-    this.onAdd(this.state.name);
-    this.setState({ name: '' });
+    const { name, number } = this.state;
+
+    this.onAdd(name, number);
+    this.setState({ name: '', number: '' });
   };
 
-  onAdd = name => {
+  onAdd = (name, number) => {
     const contact = {
       id: nanoid(),
       name,
+      number,
     };
 
     this.setState(prevState => ({
@@ -39,13 +46,14 @@ export class Phonebook extends Component {
   };
 
   render() {
-    const { name, contacts } = this.state;
+    const { name, number, contacts } = this.state;
+
     return (
       <>
         <h1>Phonebook</h1>
         <Formik initialValues={initialValues} onSubmit={this.handleSubmit}>
           <FormStyled>
-            <label htmlFor="name">Name</label>
+            <label>Name</label>
             <InputSyled
               type="text"
               name="name"
@@ -56,14 +64,27 @@ export class Phonebook extends Component {
               onChange={this.handleChange}
             />
 
+            <label>Number</label>
+            <InputSyled
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={this.handleChange}
+            />
+
             <ButtonStyle type="submit">Add contact</ButtonStyle>
           </FormStyled>
         </Formik>
 
         <h2>Contacts</h2>
         <ContactsListStyle>
-          {contacts.map(({ id, name }) => (
-            <li key={id}>{name}</li>
+          {contacts.map(({ id, name, number }) => (
+            <li key={id}>
+              {name}: {number}
+            </li>
           ))}
         </ContactsListStyle>
       </>
