18ce96b9bc3f2213ef5f7474f1de135ab8ebaa8b
Alina Cherkasova <alina.ch1202@gmail.com>
2022-10-22 22:04:32 +0300

    add: functionality to delete user

--- src/components/Phonebook/ContactList.jsx
+++ src/components/Phonebook/ContactList.jsx
@@ -1,11 +1,18 @@
 import { ContactsListStyle } from './Phonebook.styled';
 
-export const ContactList = ({ contacts }) => {
+export const ContactList = ({ contacts, deleteUser }) => {
   return (
     <ContactsListStyle>
       {contacts.map(({ id, name, number }) => (
         <li key={id}>
           {name}: {number}
+          <button
+            name={id}
+            onClick={() => deleteUser(id)}
+            style={{ marginLeft: '13px' }}
+          >
+            delete
+          </button>
         </li>
       ))}
     </ContactsListStyle>
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -6,7 +6,12 @@ import { ContactForm } from './ContactForm';
 
 export class Phonebook extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -18,14 +23,20 @@ export class Phonebook extends Component {
 
   renderFilter = contact => {
     this.state.contacts.find(el => el.name === contact.name)
-      ? this.onAlert(contact.name)
+      ? this.showAlert(contact.name)
       : this.setState(prevState => ({
           contacts: [...prevState.contacts, contact],
         }));
   };
 
-  onAlert = name => {
+  showAlert = name => {
     alert(`${name} is already in contacts`);
+  };
+
+  deleteUser = idx => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== idx),
+    }));
   };
 
   render() {
@@ -47,7 +58,7 @@ export class Phonebook extends Component {
         <Title as="h2" fontSize="1.5">
           Contacts
         </Title>
-        <ContactList contacts={renderContacts} />
+        <ContactList contacts={renderContacts} deleteUser={this.deleteUser} />
       </>
     );
   }
