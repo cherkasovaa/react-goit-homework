83a45bce5154ea9a05c20ec425289f029db92469
Alina Cherkasova <alina.ch1202@gmail.com>
2022-10-22 22:42:51 +0300

    remove: form from Filter component

--- src/components/Phonebook/Filter.jsx
+++ src/components/Phonebook/Filter.jsx
@@ -1,24 +1,15 @@
-import { FormStyled, InputSyled, Title } from './Phonebook.styled';
-import { Formik } from 'formik';
-
-const initialValues = {
-  filter: '',
-};
+import { FilterStyled, Title } from './Phonebook.styled';
 
 export const Filter = ({ value, onChange }) => {
   return (
     <>
       <Title>Find contacts by name</Title>
-      <Formik initialValues={initialValues}>
-        <FormStyled>
-          <InputSyled
-            type="text"
-            name="filter"
-            value={value}
-            onChange={onChange}
-          />
-        </FormStyled>
-      </Formik>
+      <FilterStyled
+        type="text"
+        name="filter"
+        value={value}
+        onChange={onChange}
+      />
     </>
   );
 };
