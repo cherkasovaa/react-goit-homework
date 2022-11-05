6db943711321d45dd547ae97496ec056320619b9
Alina Cherkasova <alina.ch1202@gmail.com>
2022-11-05 18:28:57 +0300

    refactor: separate ImageGalleryApp

---
+++ src/components/ImageGallery/ImageGalleryApp.jsx
@@ -0,0 +1,63 @@
+import { Component } from 'react';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Searchbar } from 'components/ImageGallery/Searchbar';
+import { AppStyled } from 'components/ImageGallery/styles';
+import { Button } from 'components/ImageGallery/Button';
+import { Loader } from 'components/ImageGallery/Loader';
+
+export class ImageGalleryApp extends Component {
+  state = {
+    query: '',
+    page: 1,
+    isLoading: false,
+    error: null,
+    items: [],
+  };
+
+  onSearch = async value => {
+    await this.setState({ isLoading: true, query: value });
+    const data = await this.getMaterials();
+    this.setState({ isLoading: false, items: data.results });
+  };
+
+  getMaterials = async () => {
+    const request = this.createRequest();
+    let response = await fetch(request);
+    let data = await response.json();
+    return data;
+  };
+
+  createRequest = () => {
+    const { query, page } = this.state;
+
+    const API_KEY = '2cqmmjl9b_w_-koNZOfoDKCv9BREiEPanFNqgtp6lAI';
+    return `https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=${query}&page=${page}`;
+  };
+
+  addImages = async e => {
+    e.preventDefault();
+    await this.setState(prevState => ({
+      page: prevState.page + 1,
+      isLoading: true,
+    }));
+
+    const images = await this.getMaterials();
+    this.setState(prevState => ({
+      items: [...prevState.items, ...images.results],
+      isLoading: false,
+    }));
+  };
+
+  render() {
+    const { items, isLoading } = this.state;
+    return (
+      // <Phonebook />
+      <AppStyled>
+        <Searchbar onSubmit={this.onSearch} />
+        {<ImageGallery items={items} />}
+        {isLoading && <Loader />}
+        {items.length > 0 && !isLoading && <Button onClick={this.addImages} />}
+      </AppStyled>
+    );
+  }
+}
