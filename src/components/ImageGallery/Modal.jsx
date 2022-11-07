a54236e8cd25bfb1f3c970bc8fc51491f6071454
Alina Cherkasova <alina.ch1202@gmail.com>
2022-11-07 21:53:49 +0300

    refactor: moal in hooks

--- src/components/ImageGallery/Modal.jsx
+++ src/components/ImageGallery/Modal.jsx
@@ -1,39 +1,69 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ModalStyled, OverlayStyled } from './styles';
 
-export class Modal extends Component {
-  componentDidMount = () => {
-    window.addEventListener('keydown', this.handleKeyDown);
-  };
-
-  componentWillUnmount = () => {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  };
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleClick = e => {
+export const Modal = ({ path, alt, onClose }) => {
+  const handleClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { path, alt } = this.props;
-    return (
-      <OverlayStyled onClick={this.handleClick}>
-        <ModalStyled>
-          <img src={path} alt={alt} />
-        </ModalStyled>
-      </OverlayStyled>
-    );
-  }
-}
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  return (
+    <OverlayStyled onClick={handleClick}>
+      <ModalStyled>
+        <img src={path} alt={alt} />
+      </ModalStyled>
+    </OverlayStyled>
+  );
+};
+
+// export class Modal extends Component {
+//   componentDidMount = () => {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   };
+
+//   componentWillUnmount = () => {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   };
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { path, alt } = this.props;
+//     return (
+//       <OverlayStyled onClick={this.handleClick}>
+//         <ModalStyled>
+//           <img src={path} alt={alt} />
+//         </ModalStyled>
+//       </OverlayStyled>
+//     );
+//   }
+// }
 
 Modal.defaultProps = {
   onClose: () => null,
