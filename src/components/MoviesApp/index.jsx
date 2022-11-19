07c446aced108f63b7e49d1455879e45e7772025
Alina Cherkasova <alina.ch1202@gmail.com>
2022-11-19 19:08:27 +0300

    add: display movie app

---
+++ src/components/MoviesApp/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import { Home } from './pages/Home';
+import { MovieInfo } from './pages/MovieInfo';
+import { Movies } from './pages/Movies';
+import * as S from './styles';
+import { theme } from './variables';
+
+// TODO: create homepage template
+// TODO: create post template and style
+// TODO: render thrending movies from API
+// TODO: render search movies
+// TODO: add back button
+// TODO: add color theme
+
+export const MoviesApp = () => {
+  const navLinksArray = [
+    { name: 'home', path: '/', page: <Home /> },
+    { name: 'movies', path: '/movies', page: <Movies /> },
+  ];
+
+  return (
+    <ThemeProvider theme={theme}>
+      <S.Container>
+        <Header links={navLinksArray} />
+        <Routes>
+          {navLinksArray.map(({ name, path, page }) => (
+            <Route key={name} path={path} element={page} />
+          ))}
+          <Route path="/:id" element={<MovieInfo />} />
+        </Routes>
+      </S.Container>
+    </ThemeProvider>
+  );
+};
