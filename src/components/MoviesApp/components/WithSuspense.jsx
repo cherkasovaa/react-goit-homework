30f2a2e9a22dfd55d1fa9b12aea1ae1b10982156
Alina Cherkasova <alina.ch1202@gmail.com>
2022-11-25 22:24:15 +0300

    add: wrapper component WithSuspense

---
+++ src/components/MoviesApp/components/WithSuspense.jsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from 'react';
+import { Loader } from './Loader';
+
+const WithSuspense = Component => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Component />;
+    </Suspense>
+  );
+};
+
+export default WithSuspense;
--- src/components/MoviesApp/index.jsx
+++ src/components/MoviesApp/index.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { lazy, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
-import { Cast } from './components/Cast';
-import { Reviews } from './components/Reviews';
-import { Movies } from './pages/Movies';
-import MovieInfo from './pages/MovieInfo';
-import Home from './pages/Home/Home';
+import WithSuspense from './components/WithSuspense';
 
 import * as S from './styles';
 import { theme } from './variables';
+
+const Cast = lazy(() => import('./components/Cast'));
+const Reviews = lazy(() => import('./components/Reviews'));
+const Home = lazy(() => import('./pages/Home/Home'));
+const Movies = lazy(() => import('./pages/Movies'));
+const MovieInfo = lazy(() => import('./pages/MovieInfo'));
 
 export const MoviesApp = () => {
   const [isDark, setIsDark] = useState(
@@ -31,16 +33,16 @@ export const MoviesApp = () => {
         <Header links={navLinksArray} checkTheme={checkTheme} />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<MovieInfo />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+          <Route path="/" element={WithSuspense(Home)} />
+          <Route path="/:id" element={WithSuspense(MovieInfo)}>
+            <Route path="cast" element={WithSuspense(Cast)} />
+            <Route path="reviews" element={WithSuspense(Reviews)} />
           </Route>
 
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:id" element={<MovieInfo />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+          <Route path="/movies" element={WithSuspense(Movies)} />
+          <Route path="/movies/:id" element={WithSuspense(MovieInfo)}>
+            <Route path="cast" element={WithSuspense(Cast)} />
+            <Route path="reviews" element={WithSuspense(Reviews)} />
           </Route>
         </Routes>
       </S.Container>
