62da4a9c7f7c8efe527ab4456e978368334cc878
Alina Cherkasova <alina.ch1202@gmail.com>
2022-11-26 13:28:26 +0300

    fix: width 100%  movie app

--- src/components/MoviesApp/styles.js
+++ src/components/MoviesApp/styles.js
@@ -4,4 +4,5 @@ export const Container = styled.div`
   background-color: ${props => props.theme.background};
   color: ${props => props.theme.primaryFontColor};
   min-height: 100vh;
+  width: 100%;
 `;
